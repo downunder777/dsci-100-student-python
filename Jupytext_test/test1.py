95368707a3c5299505ac17ea2c850303fec0806f
nv <115237413+goldminenv@users.noreply.github.com>
2026-06-21 16:34:38 +1000

    created paired notebook

--- Jupytext_test/test1.py
+++ Jupytext_test/test1.py
@@ -8,7 +8,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.3
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
